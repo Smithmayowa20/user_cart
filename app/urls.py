6d633bdddc69d1eb7a56958e821e6129394c6ef6
Smith <smith@Onyekas-MacBook-Air.local>
2022-03-06 16:55:53 +0000

    added store detail page

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from app.views import index
+from app.views import index, store_detail
 
 
 urlpatterns = [
     path('', index),
+    path('store/<store_link>/', store_detail, name="store_detail")
 ]
--- app/views.py
+++ app/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-from datetime import datetime
+from django.core.paginator import Paginator
 
 from django.http import HttpResponse
 
 import csv
+
+import requests
 
 file = "app/Top 10,000 Shopify Stores Dataset - October 2020.csv"
 
@@ -14,13 +16,26 @@ with open(file, newline='') as f:
     data = list(reader)
 
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello from Vercel!</h1>
-            <p>The current time is { now }.</p>
-        </body>
-    </html>
-    '''
-    return render(request, 'index.html', {})
+    p = Paginator(data[1:], 100)
+    # getting the desired page number from url
+    page_number = request.GET.get('page')
+    try:
+        page_obj = p.get_page(page_number)
+    except PageNotAnInteger:
+        page_obj = p.page(1)
+    except EmptyPage:
+        page_obj = p.page(p.num_pages)
+    context = {'page_obj': page_obj}
+    # sending the page object to index.html
+    return render(request, 'index.html', context)
+
+
+def store_detail(request,store_link):
+    
+    store_collections = requests.get("https://www.{store_link}/collections.json?limit=250&page=0".format(store_link=store_link)).json()
+    store_products =  requests.get("https://www.{store_link}/products.json?limit=250&page=0".format(store_link=store_link)).json()
+
+    print(store_products['products'][0]['images'])
+    context = {'store_products': store_products['products'], 'store_collections':store_collections['collections'], 'store_link':store_link}
+    # sending the page object to index.html
+    return render(request, 'store_detail.html', context)
